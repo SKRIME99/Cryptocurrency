5af4f456c7e5562b5b795354ec82c92a673aa5c7
alexey <kompalexey@gmail.com>
2024-04-03 18:57:04 +0300

    fixed smells

--- src/main/java/com/coinsearch/CryptoserviceApplication.java
+++ src/main/java/com/coinsearch/CryptoserviceApplication.java
@@ -3,7 +3,7 @@ package com.coinsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication// во время запуска оно автоконфигурирует как спринг бут апликейшен
+@SpringBootApplication// во время запуска оно автоконфигурирует как спринг бут
 public class CryptoserviceApplication {
 
 	public static void main(String[] args) {//могу использовать в других классах не создавая обьекты этогоо класса
