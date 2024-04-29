442d610121b70fa41121ec087be695ea2851afc6
alexey <kompalexey@gmail.com>
2024-04-29 06:24:00 +0300

    lab7

--- src/main/java/com/coinsearch/AppConfig.java
+++ src/main/java/com/coinsearch/AppConfig.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
